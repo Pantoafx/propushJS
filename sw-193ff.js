2b1e69bfadbdeaa1deaab4bbb5d4325de6093fee
PantoaFx <pantoafx@gmail.com>
2019-10-18 18:37:31 +0800

    Add files via upload

---
+++ sw-193ff.js
@@ -0,0 +1,6 @@
+self.options = {
+    "domain": "jootizud.net",
+    "zoneId": 2883064
+}
+self.lary = ""
+importScripts('https://jootizud.net/pfe/current/service-worker.min.js?r=sw')
---
+++ sw-dd9d8.js
@@ -0,0 +1,6 @@
+self.options = {
+    "domain": "jootizud.net",
+    "zoneId": 2883063
+}
+self.lary = ""
+importScripts('https://jootizud.net/pfe/current/service-worker.min.js?r=sw')
